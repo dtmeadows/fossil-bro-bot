d60ca29c60ec650485da621954c7448b4cbeb9a8
D Meadows <davidtmeadows@gmail.com>
2020-08-14 14:19:55 -0700

    add tests

---
+++ handle_message_content.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const commands = new Map();
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+const { prefix } = require('./config.json');
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.set(command.name, command);
+}
+
+function handleMessageContent(content) {
+  const trimmedMessage = content.trimLeft();
+
+  // check if the prefix is at the start of the message
+  if (!trimmedMessage.startsWith(prefix)) {
+    return undefined;
+  }
+
+  // a valid command is any non space character following the prefix
+  // we also will optionally capture any of the text following the command in case
+  //
+  const commandExtractRegexp = new RegExp(`${prefix}(?<commandName>\\S+)\\s?(?<contentAfterCommand>.*)?`);
+  const regexpExtract = commandExtractRegexp.exec(trimmedMessage);
+  if (regexpExtract === null) {
+    // regex didn't match syntax
+    // todo return a helpful error here
+    return undefined;
+  }
+
+  const { commandName, contentAfterCommand } = regexpExtract.groups;
+
+  if (!commands.has(commandName)) {
+    console.log(`no command found for ${commandName}`);
+    return undefined;
+  }
+  console.log(`running command ${commandName}`);
+  const outputMessage = commands.get(commandName).execute(contentAfterCommand);
+  console.log(`got output message: ${outputMessage}`);
+  return outputMessage;
+}
+
+exports.handleMessageContent = handleMessageContent;
