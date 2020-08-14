ccf001949bca93fad519c67f540859c6aae6ebbd
D Meadows <davidtmeadows@gmail.com>
2020-08-14 14:19:25 -0700

    'downvote'

--- commands/downvote.js
+++ commands/downvote.js
@@ -1,9 +1,24 @@
 module.exports = {
   name: '--',
-  description: 'Downvote something',
+  description: 'Upvote something',
   execute(message) {
-    const regex = /(?<recipient>\w+)\s+(?<reason>.*)/;
-    const matches = regex.exec(message);
-    return `-- ${matches.recipient} ${matches.reason} (now at XX)`;
+    const regex = /(?<recipient>\w+)\s*(?<reason>.*)?/;
+    const regexpExtract = regex.exec(message);
+
+    if (regexpExtract === null) {
+      // todo add better error message
+      return 'invalid format for -- command';
+    }
+
+    // this will return either:
+    // `-- recipient reason (now at xx)`
+    // or if the reason is null
+    // `-- recipient (now at xx)
+    return [
+      '--',
+      regexpExtract.groups.recipient,
+      regexpExtract.groups.reason,
+      '(now at XX)',
+    ].filter(Boolean).join(' ');
   },
 };
