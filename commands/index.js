a54c4d63db7b9e9465244a2abfbfb00235d79de2
D Meadows <davidtmeadows@gmail.com>
2020-08-19 21:33:06 -0700

    fixup!

--- commands/index.js
+++ commands/index.js
@@ -4,6 +4,7 @@ const downvote = require('./downvote.js');
 const ping = require('./ping.js');
 const checkKarma = require('./check_karma.js');
 const help = require('./help.js');
+const karmaStats = require('./karma_stats.js')
 
 const commands = new Discord.Collection();
 
@@ -12,5 +13,6 @@ commands.set(downvote.name, downvote);
 commands.set(checkKarma.name, checkKarma);
 commands.set(ping.name, ping);
 commands.set(help.name, help);
+commands.set(karmaStats.name, karmaStats);
 
 exports.commands = commands;
