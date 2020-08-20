eb82661e69414cfdfc973c3e93affa4df77b30f3
dtmeadows <davidtmeadows@gmail.com>
2020-08-19 23:16:22 -0700

    better stats output (#23)

--- test/unit/commands/pong.js
+++ test/unit/commands/pong.js
@@ -4,7 +4,6 @@ const assert = require('assert');
 const pong = require('../../../commands/pong');
 const checkKarma = require('../../../commands/check_karma');
 
-
 describe('pong', () => {
   it('decrements someone', async () => {
     assert.equal(await pong.execute('<@!138508771897901066>', 'server_abc123', '138508771897901066'), 'Results did not attain acceptable levels of certainty. Try again?');
