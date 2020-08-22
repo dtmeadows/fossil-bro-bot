44a9a3d16cb25388de649911499c128e8052c6d6
dtmeadows <davidtmeadows@gmail.com>
2020-08-22 15:02:17 -0700

    Handle nickname mention in reason (#32)
    * replaced nickname mentions
    
    * fixed

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
+async function getDisplayName(userId, message) {
+  let guildMember = null;
+  guildMember = message.guild.members.cache.get(userId);
+  if (!guildMember) {
+    guildMember = await message.guild.members.fetch(userId).catch(() => { });
+  }
+
+  let user = null;
+  if (guildMember) {
+    user = guildMember.user;
+  } else {
+    user = await message.mentions.users.get(userId);
+  }
+
+  // Prefer, in order:
+  // 1. Per-server nickname
+  // 2. Username (eg @username)
+  // 3. Just the user ID
+  let displayName = null;
+  if (guildMember && guildMember.nickname) {
+    displayName = guildMember.nickname;
+  } if (user && user.username) {
+    displayName = user.username;
+  } else {
+    displayName = userId;
+  }
+  return `\`@${displayName}\``;
+}
+
 // this takes any bot response and:
 // -> takes any mention and re-formats it to be the same
 //  content but wrapped in ``. This is to prevent the bot
@@ -15,48 +44,18 @@ client.once('ready', () => {
 //  commands would do that already. This also makes it so
 //  calling a command like stats doesn't notify 10 people
 //  at once.
-async function formatResponse(response, message) {
-  const mentionRegex = /<@!?(?<userId>\d+)>/g;
-  const extract = Array.from(response.matchAll(mentionRegex));
-
-  let formattedResponse = null;
-  if (extract && extract.length > 0) {
-    formattedResponse = response;
-    extract.forEach(async (ex) => {
-      const { userId } = ex.groups;
-
-      let guildMember = null;
-      guildMember = message.guild.members.cache.get(userId);
-      if (!guildMember) {
-        guildMember = message.guild.members.fetch(userId).then((r) => r).catch(() => { });
-      }
-
-      let user = null;
-      if (guildMember) {
-        user = guildMember.user;
-      } else {
-        user = message.mentions.users.get(userId);
-      }
-
-      // Prefer, in order:
-      // 1. Per-server nickname
-      // 2. Username (eg @username)
-      // 3. Just the user ID
-      let displayName = null;
-      if (guildMember && guildMember.nickname) {
-        displayName = guildMember.nickname;
-      } else if (user && user.username) {
-        displayName = user.username;
-      } else {
-        displayName = userId;
-      }
-
-      formattedResponse = formattedResponse.replace(`<@${userId}>`, `\`@${displayName}\``);
-    });
-  } else {
-    formattedResponse = response;
-  }
-  return formattedResponse;
+async function replaceMentions(response, message) {
+  const regex = /<@!?(\d+)>/g;
+  // these manual promise operations are necessary because
+  // by default .replace() doesn't handle promises.
+  // answer taken from https://stackoverflow.com/a/48032528
+  const promises = [];
+  response.replace(regex, async (_match, p1) => {
+    const promise = getDisplayName(p1, message);
+    promises.push(promise);
+  });
+  const data = await Promise.all(promises);
+  return response.replace(regex, () => data.shift());
 }
 
 async function handleMessage(message) {
@@ -71,9 +70,8 @@ async function handleMessage(message) {
     );
 
     if (response) {
-      const formattedResponse = await formatResponse(response, message);
+      const formattedResponse = await replaceMentions(response, message);
 
-      console.log(`sending response: ${formattedResponse}`);
       message.channel.send(formattedResponse);
     }
   } catch (error) {
